a6c998fdfd620310ea028c9dda47bad8e3120571
Mohit <mohit.sharma0690@gmail.com>
2018-11-15 18:24:27 -0500

    Remove ROS from run loop interface.

--- src/catkin_ws/src/robot_interface/include/robot_interface/run_loop.h
+++ src/catkin_ws/src/robot_interface/include/robot_interface/run_loop.h
@@ -1,19 +1,29 @@
 #pragma once
 
-#include <ros/node_handle.h>
-#include <ros/time.h>
+#include <cmath>
+#include <functional>
+
+#include <libiam/motion_generator.h>
+
+void setCurrentThreadToRealtime(bool throw_on_error);
 
 // TODO(Mohit): Add a namespace to these declarations.
 // TODO(Mohit): Need to make this an interface.
 class RunLoop {
  public:
   // TODO(Mohit): Maybe we should pass in a pointer to the main loop interface?
-  bool init(ros::NodeHandle& node_handle);
-  void starting(const ros::Time&);
-  void update(const ros::Time&, const ros::Duration& period);
+  bool init();
+  void start();
+  void stop();
+  void update();
   void run();
 
  private:
-  ros::Duration elapsed_time_;
-  ros::NodeHandle& node_handle;
+
+  MotionGenerator motion_generator;
+
+  const bool limit_rate_;  // NOLINT(readability-identifier-naming)
+  const double cutoff_frequency_; // NOLINT(readability-identifier-naming)
+  uint32_t elapsed_time_;
+
 };
--- src/catkin_ws/src/robot_interface/src/main_loop.cpp
+++ src/catkin_ws/src/robot_interface/src/main_loop.cpp
@@ -1,60 +1,53 @@
-#include <ros/ros.h>
-#include <signal.h>
+#include "run_loop.h"
 
-#include <actionlib/server/simple_action_server.h>
-#include <robot_interface/execute_skillAction.h>
-#include <robot_interfae/run_loop.h>
+#include <pthread.h>
 
-bool RunLoop::init(ros::NodeHandle& nh): node_handle(nh) {
-}
-
-void RunLoop::starting(const ros::Time&) {
-}
-
-void RunLoop::update(const ros::Time& /* time */,
-                     const ros::Duration& period) {
-}
-
-void mySigintHandler(int sig)
-{
-  // Do some custom action.
-  // For example, publish a stop message to some other nodes.
-  
-  // All the default sigint handler does is call shutdown()
-  ros::shutdown();
-}
-
-// TODO: This main loop should be inside some main runner class.
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "robot_interface_main_node");
-  ros::NodeHandle nh;
-
-  // Read parameters from param server.
-  //
-  // Instantiate new skills (Note that this should happen in the loop)
-  // TODO: How do we instantiate new skills on the fly?
-  execute_skillAction exec_skill_action("skill")
+#include <cerrno>
+#include <cstring>
+#include <exception>
+#include <fstream>
+#include <iostream>
 
 
-  // Override the default ros sigint handler.
-  // This must be set after the first NodeHandle is created.
-  // signal(SIGINT, mySigintHandler);
-
-  int count = 0;
-  ros::Rate loop_rate(1000);
-  while (ros::ok()) {
-    ros::Time last_time = ros::Time::now();
-
-
-    if (count % 100 == 0) {
-      // Publish feedback message
-      count = 0
-    }
-    ros::spinOnce();
-    loop_rate.sleep();
-    ++count;
+void setCurrentThreadToRealtime(bool throw_on_error) {
+  // Change prints to exceptions.
+  const int thread_priority = sched_get_priority_max(SCHED_FIFO);
+  if (thread_priority == -1) {
+    cout << "libfranka: unable to get maximum possible thread priority: "s +
+        std::strerror(errno);
   }
-  
-  return 0;
+  sched_param thread_param{};
+  thread_param.sched_priority = thread_priority;
+  if (pthread_setschedparam(pthread_self(), SCHED_FIFO, &thread_param) != 0) {
+    if (throw_on_error) {
+      cout << "libfranka: unable to set realtime scheduling: "s +
+          std::strerror(errno);
+    }
+  }
+}
+
+bool RunLoop::init() {
+  // TODO(Mohit): Initialize memory and stuff.
+  bool throw_on_error;
+  setCurrentThreadToRealtime(throw_on_error)
+}
+
+void RunLoop::start() {
+  // Start processing, might want to do some pre-processing 
+  cout << ("Will start run loop.\n";
+}
+
+void RunLoop::stop() {
+  // Maybe call this after exceptions or SIGINT or any Interrupt.
+  // Stop the interface gracefully.
+}
+
+void RunLoop::update() {
+  cout << "update run loop\n";
+}
+
+void RunLoop::run() {
+  while (1) {
+    update();
+  }
 }
