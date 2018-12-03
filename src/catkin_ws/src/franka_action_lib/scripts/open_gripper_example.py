c715fdc34ab05fe6377f8235909d823463c76c0b
Mohit <mohit.sharma0690@gmail.com>
2018-12-03 13:55:18 -0500

    Add more modular client side code.

--- src/catkin_ws/src/franka_action_lib/scripts/open_gripper_example.py
+++ src/catkin_ws/src/franka_action_lib/scripts/open_gripper_example.py
@@ -7,6 +7,9 @@ import actionlib
 
 from franka_action_lib.msg import ExecuteSkillAction, ExecuteSkillGoal
 
+from skill_list import BaseSkill
+from skill_list import OpenGripperWithDefaultSensorSkill
+
 def feedback_callback(feedback):
     print(feedback)
 
@@ -15,34 +18,12 @@ if __name__ == '__main__':
     client = actionlib.SimpleActionClient('/execute_skill_action_server_node/execute_skill', ExecuteSkillAction)
     client.wait_for_server()
 
-    goal = ExecuteSkillGoal()
-    # Fill in the goal here
-    initial_sensor_values = [1,3,5,7,9]
-    traj_gen_params = [0.05,0.025,2100]
-    feedback_controller_params = [];
-    termination_params = [];
-    
-    timer_params = [1,2,3,4,5];
+    skill = OpenGripperWithDefaultSensorSkill(1)
+    skill.add_initial_sensor_values([1, 3, 5, 7, 8])  # random
+    skill.add_trajectory_params([0.05, 0.025, 2100])
+    goal = client.create_goal()
+    client.send_goal(goal, feedback_cb=lambda x: skill.feedback_callback(x))
 
-    goal.skill_type = 1
-    goal.sensor_topics = ["/franka_robot/camera/"]
-    goal.sensor_value_sizes = [len(initial_sensor_values)]
-    goal.initial_sensor_values = initial_sensor_values
-    goal.traj_gen_type = 5
-    goal.num_traj_gen_params = len(traj_gen_params)
-    goal.traj_gen_params = traj_gen_params
-    goal.feedback_controller_type = 1
-    goal.num_feedback_controller_params = len(feedback_controller_params)
-    goal.feedback_controller_params = feedback_controller_params
-    goal.termination_type = 1
-    goal.num_termination_params = len(termination_params)
-    goal.termination_params = termination_params
-    goal.timer_type = 1
-    goal.num_timer_params = len(timer_params)
-    goal.timer_params = timer_params
-
-    client.send_goal(goal, feedback_cb=feedback_callback)
-    done = client.wait_for_result(rospy.Duration.from_sec(5.0))
 
     while not rospy.is_shutdown() and done != True:
         done = client.wait_for_result(rospy.Duration.from_sec(5.0))
---
+++ src/catkin_ws/src/franka_action_lib/scripts/skill_list.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import roslib
+roslib.load_manifest('franka_action_lib')
+import rospy
+import actionlib
+import numpy as np
+
+from franka_action_lib.msg import ExecuteSkillAction, ExecuteSkillGoal
+
+class BaseSkill(object):
+    def __init__(self, 
+                 skill_type,
+                 sensor_topics,
+                 trajectory_generator_type,
+                 feedback_controller_type,
+                 termination_type,
+                 timer_type):
+        super(BaseSkill, self).__init__()
+        self._skill_type = skill_type
+        self._sensor_topics = sensor_topics
+        self._trajectory_generator_type = trajectory_generator_type
+        self._feedback_controller_type = feedback_controller_type
+        self._termination_type = termination_type
+        self._timer_type = timer_type
+
+        self._sensor_value_sizes = 0
+        self._initial_sensor_values = []
+
+        # Add trajectory params
+        self._trajectory_generator_params = []
+        self._num_trajectory_generator_params = 0
+
+        # Add feedback controller params
+        self._feedback_controller_params = []
+        self._num_feedback_controller_params = 0
+
+        # Add termination params
+        self._termination_params = []
+        self._num_termination_params = 0
+
+        # Add timer params
+        self._timer_params = []
+        self._num_timer_params = 0
+
+
+    def add_initial_sensor_values(self, values):
+        self._initial_sensor_values = values
+        self._sensor_value_sizes = [len(values)]
+
+    def add_trajectory_params(self, params):
+        self._trajectory_generator_params = params
+        self._num_trajectory_generator_params = len(params)
+
+    def add_feedback_controller_params(self, params):
+        self._feedback_controller_params = params
+        self._num_feedback_controller_params = len(params)
+
+    def add_termination_params(self, params):
+        self._termination_params = params
+        self._num_termination_params = len(params)
+
+    def add_timer_params(self, params):
+        self._timer_params = params
+        self._num_timer_params = len(params)
+
+    # Add checks for these
+    def create_goal(self):
+        goal = ExecuteSkillGoal()
+        goal.skill_type = self._skill_type
+        goal.sensor_topics = self._sensor_topics
+        goal._initial_sensor_values = self._initial_sensor_values
+        goal.traj_gen_type = self._trajectory_generator_type
+        goal.traj_gen_params = self._trajectory_generator_params
+        goal.num_traj_gen_params = self._num_trajectory_generator_params
+        goal.feedback_controller_type = self._feedback_controller_type
+        goal.feedback_controller_params = self._feedback_controller_params
+        goal.num_feedback_controller_params = \
+                self._feedback_controller_params
+        goal.termination_type = self._termination_type
+        goal.termination_params = self._termination_params
+        goal.num_termination_params = self._num_termination_params
+        goal.timer_type = self._timer_type
+        goal.timer_params = self._timer_params
+        goal.num_timer_params = self._num_timer_params
+
+    def feedback_callback(self, feedback):
+        print(feedback)
+
+class OpenGripperWithDefaultSensorSkill(BaseSkill):
+    def __init_(self, 
+                skill_type,
+                trajectory_generator_type=5,
+                feedback_controller_type=1,
+                termination_type=1,
+                timer_type=1):
+        super(OpenGripperSkill, self).__init_(
+              skill_type,
+              sensor_topics=['/franka_robot/camera'],
+              trajectory_generator_type,
+              feedback_controller_type,
+              termination_type,
+              timer_type)
