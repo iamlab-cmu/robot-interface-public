355f4bf3c014fcee39eeb2e4664fb8c8104be0b8
Mohit <mohit.sharma0690@gmail.com>
2019-01-17 15:56:07 -0500

    temp commit

--- src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_iam_robolib_on_control_pc.sh
+++ src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_iam_robolib_on_control_pc.sh
@@ -4,13 +4,14 @@ control_pc_uname=$1
 control_pc_ip_address=$2
 workstation_ip_address=$3
 control_pc_use_passwd=$4
+control_pc_robot_lib_path="~/projects/robot-interface"
 
-if [ "$(control_pc_use_passwd)" -eq 0 ]; then
+if [ "$control_pc_use_passwd" = "0" ]; then
 ssh -T $control_pc_uname@$control_pc_ip_address << EOSSH
-  ~/Documents/robot-interface/build/test_iam_robolib
+$control_pc_robot_lib_path"/build/test_iam_robolib"
 EOSSH
 else
 sshpass -p "!@m-guardians" ssh -tt -o StrictHostKeyChecking=no $control_pc_uname@$control_pc_ip_address << EOSSH
-  ~/Documents/robot-interface/build/test_iam_robolib
+~/Documents/robot-interface/build/test_iam_robolib
 EOSSH
 fi
--- src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_ros_action_lib_on_control_pc.sh
+++ src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_ros_action_lib_on_control_pc.sh
@@ -4,11 +4,14 @@ control_pc_uname=$1
 control_pc_ip_address=$2
 workstation_ip_address=$3
 control_pc_use_passwd=$4
+control_pc_robot_lib_path="~/projects/robot-interface"
 
-if [ "$(control_pc_use_passwd)" -eq 0 ]; then
+rosmaster_path="/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh"
+
+if [ "$control_pc_use_passwd" = "0" ]; then
 ssh -T $control_pc_uname@$control_pc_ip_address << EOSSH
-source ~/Documents/robot-interface/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh $control_pc_ip_address $workstation_ip_address
-source ~/Documents/robot-interface/src/catkin_ws/devel/setup.bash
+source $control_pc_robot_lib_path$rosmaster_path $control_pc_ip_address $workstation_ip_address
+source $control_pc_robot_lib_path"/src/catkin_ws/devel/setup.bash"
 roslaunch franka_action_lib execute_skill_action_server_node.launch
 EOSSH
 else
