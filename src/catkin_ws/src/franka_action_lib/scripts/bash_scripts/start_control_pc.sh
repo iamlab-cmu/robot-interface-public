e34e0dd11ed032a34b74b36e41d1dbabdee634fb
Kevin Zhang <kevinleezhang@gmail.com>
2019-01-16 21:08:52 -0500

    Fixed start control pc script.

--- src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_control_pc.sh
+++ src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_control_pc.sh
@@ -9,19 +9,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 start_rosmaster_path="$DIR/start_rosmaster.sh"
 gnome-terminal -e "bash $start_rosmaster_path"
 
-sleep 19
+sleep 5
 
 # Ssh to the control pc and start iam_robolib in a new gnome-terminal
 start_iam_robolib_on_control_pc_path="$DIR/start_iam_robolib_on_control_pc.sh"
 gnome-terminal -e "bash $start_iam_robolib_on_control_pc_path $control_pc_ip_address $workstation_ip_address"
 
-sleep 19
+sleep 5
 
 # Ssh to the control pc and start ROS action server in a new gnome-terminal
 start_ros_action_lib_on_control_pc_path="$DIR/start_ros_action_lib_on_control_pc.sh"
 gnome-terminal -e "bash $start_ros_action_lib_on_control_pc_path $control_pc_ip_address $workstation_ip_address"
 
-sleep 19
+sleep 5
 
 # Start realsense camera on the workstation pc in a new gnome-terminal
 start_realsense_on_workstation_path="$DIR/start_realsense_on_workstation.sh"
--- src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_ros_action_lib_on_control_pc.sh
+++ src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_ros_action_lib_on_control_pc.sh
@@ -4,7 +4,7 @@ control_pc_ip_address=$1
 workstation_ip_address=$2
 
 sshpass -p "!@m-guardians" ssh -tt -o StrictHostKeyChecking=no iam-lab@$control_pc_ip_address << EOSSH
-	bash ~/Documents/robot-interface/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh $control_pc_ip_address $workstation_ip_address
+	source ~/Documents/robot-interface/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh $control_pc_ip_address $workstation_ip_address
 	source ~/Documents/robot-interface/src/catkin_ws/devel/setup.bash
 	roslaunch franka_action_lib execute_skill_action_server_node.launch
 EOSSH
