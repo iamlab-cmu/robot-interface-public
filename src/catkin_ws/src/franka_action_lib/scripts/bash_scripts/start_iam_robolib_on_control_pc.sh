b91f867819db2a954b8ca0f754619c91df221d98
Mohit <mohit.sharma0690@gmail.com>
2019-01-17 15:39:05 -0500

    Improve the automoated running scripts.

--- src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_iam_robolib_on_control_pc.sh
+++ src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_iam_robolib_on_control_pc.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-control_pc_ip_address=$1
-workstation_ip_address=$2
+control_pc_uname=$1
+control_pc_ip_address=$2
+workstation_ip_address=$3
+control_pc_use_passwd=$4
 
-sshpass -p "!@m-guardians" ssh -tt -o StrictHostKeyChecking=no iam-lab@$control_pc_ip_address << EOSSH
-	~/Documents/robot-interface/build/test_iam_robolib
+if [ "$(control_pc_use_passwd)" -eq 0 ]; then
+ssh -T $control_pc_uname@$control_pc_ip_address << EOSSH
+  ~/Documents/robot-interface/build/test_iam_robolib
 EOSSH
+else
+sshpass -p "!@m-guardians" ssh -tt -o StrictHostKeyChecking=no $control_pc_uname@$control_pc_ip_address << EOSSH
+  ~/Documents/robot-interface/build/test_iam_robolib
+EOSSH
+fi
--- src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_ros_action_lib_on_control_pc.sh
+++ src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_ros_action_lib_on_control_pc.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-control_pc_ip_address=$1
-workstation_ip_address=$2
+control_pc_uname=$1
+control_pc_ip_address=$2
+workstation_ip_address=$3
+control_pc_use_passwd=$4
 
-sshpass -p "!@m-guardians" ssh -tt -o StrictHostKeyChecking=no iam-lab@$control_pc_ip_address << EOSSH
-	source ~/Documents/robot-interface/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh $control_pc_ip_address $workstation_ip_address
-	source ~/Documents/robot-interface/src/catkin_ws/devel/setup.bash
-	roslaunch franka_action_lib execute_skill_action_server_node.launch
+if [ "$(control_pc_use_passwd)" -eq 0 ]; then
+ssh -T $control_pc_uname@$control_pc_ip_address << EOSSH
+source ~/Documents/robot-interface/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh $control_pc_ip_address $workstation_ip_address
+source ~/Documents/robot-interface/src/catkin_ws/devel/setup.bash
+roslaunch franka_action_lib execute_skill_action_server_node.launch
 EOSSH
+else
+sshpass -p "!am-guardians" ssh -tt -o StrictHostKeyChecking=no $control_pc_uname@$control_pc_ip_address << EOSSH
+source ~/Documents/robot-interface/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh $control_pc_ip_address $workstation_ip_address
+source ~/Documents/robot-interface/src/catkin_ws/devel/setup.bash
+roslaunch franka_action_lib execute_skill_action_server_node.launch
+EOSSH
+fi
