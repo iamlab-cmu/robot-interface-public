f263ee14a4a8abf1100ef12bf877ffef1461f80e
Mohit <mohit.sharma0690@gmail.com>
2019-01-19 06:11:22 -0500

    Add launch file to extract images from rosbag.

--- workstatation/utils/rosbag_utils.py
+++ workstatation/utils/rosbag_utils.py
@@ -1,8 +1,8 @@
 import rosbag
 import ipdb
 
-bag = rosbag.Bag('~/try_1/cutting_rosbag_2019-01-18-18-27-25.bag')
+bag = rosbag.Bag('/home/mohit/try_1/cutting_rosbag_2019-01-18-18-27-25.bag')
 ipdb.set_trace()
 for topic, msg, t in bag.read_messages(topics=['chatter', 'numbers']):
-    print msg
+    print(msg)
     bag.close()
