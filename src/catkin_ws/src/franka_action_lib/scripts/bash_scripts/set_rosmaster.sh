4034990ce541a8844eaafab03ec1dccd5dc0c42f
Kevin Zhang <kevinleezhang@gmail.com>
2019-01-22 13:29:00 -0500

    Fixed set_rosmaster bash script.

--- src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh
+++ src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh
@@ -4,5 +4,5 @@ control_pc_ip_address=$1
 workstation_ip_address=$2
 
 export ROS_MASTER_URI="http://$workstation_ip_address:11311"
-export ROS_HOSTNAME="$control_pc_ip_address"
-export ROS_IP="$control_pc_ip_address"
+export ROS_HOSTNAME="${control_pc_ip_address}"
+export ROS_IP="${control_pc_ip_address}"
