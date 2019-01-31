7772943fe0cc82f1fb87157416e63d8eaa95c24e
Kevin Zhang <kevinleezhang@gmail.com>
2019-01-31 17:26:14 -0500

    Add robot state data to launch file.

--- src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_ros_action_lib_on_control_pc.sh
+++ src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/start_ros_action_lib_on_control_pc.sh
@@ -12,12 +12,12 @@ if [ "$control_pc_use_passwd" = "0" ]; then
 ssh -T $control_pc_uname@$control_pc_ip_address << EOSSH
 source $control_pc_robot_lib_path$rosmaster_path $control_pc_ip_address $workstation_ip_address
 source $control_pc_robot_lib_path"/src/catkin_ws/devel/setup.zsh"
-roslaunch franka_action_lib execute_skill_action_server_node.launch
+roslaunch franka_action_lib franka_ros_interface.launch
 EOSSH
 else
 sshpass -p "!am-guardians" ssh -tt -o StrictHostKeyChecking=no $control_pc_uname@$control_pc_ip_address << EOSSH
 source ~/Documents/robot-interface/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh $control_pc_ip_address $workstation_ip_address
 source ~/Documents/robot-interface/src/catkin_ws/devel/setup.bash
-roslaunch franka_action_lib execute_skill_action_server_node.launch
+roslaunch franka_action_lib franka_ros_interface.launch
 EOSSH
 fi
