c3a663276c4d4b8a33757af53d5ea176abf84ff1
Kevin Zhang <kevinleezhang@gmail.com>
2019-03-19 17:52:32 -0400

    Used static cast to make the parameter parsing more secure for the custom_gain_torque_controller.

--- iam_robolib/include/iam_robolib/feedback_controller/custom_gain_torque_controller.h
+++ iam_robolib/include/iam_robolib/feedback_controller/custom_gain_torque_controller.h
@@ -26,7 +26,8 @@ class CustomGainTorqueController : public FeedbackController {
   std::array<double, 7> k_gains_ = {{600.0, 600.0, 600.0, 600.0, 
                                      250.0, 150.0, 50.0}};
   // Damping
-  std::array<double, 7> d_gains_ = {{50.0, 50.0, 50.0, 50.0, 30.0, 25.0, 15.0}};
+  std::array<double, 7> d_gains_ = {{50.0, 50.0, 50.0, 50.0, 
+                                     30.0, 25.0, 15.0}};
 };
 
 #endif  // IAM_ROBOLIB_FEEDBACK_CONTROLLER_CUSTOM_GAIN_TORQUE_CONTROLLER_H_
