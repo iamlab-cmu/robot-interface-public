4aa8b74a184d9c8958b0436a2729f32062d8f37e
Kevin Zhang <kevinleezhang@gmail.com>
2019-03-19 22:07:35 -0400

    Modified some bash scripts due to new folder locations.

--- bash_scripts/start_ros_action_lib_on_control_pc.sh
+++ bash_scripts/start_ros_action_lib_on_control_pc.sh
@@ -6,7 +6,7 @@ workstation_ip_address=$3
 control_pc_use_passwd=$4
 control_pc_robot_lib_path=$5
 
-rosmaster_path="/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh"
+rosmaster_path="/bash_scripts/set_rosmaster.sh"
 
 echo $control_pc_uname
 echo $control_pc_ip_address
@@ -15,13 +15,13 @@ echo $control_pc_robot_lib_path
 if [ "$control_pc_use_passwd" = "0" ]; then
 ssh -T $control_pc_uname@$control_pc_ip_address << EOSSH
 source $control_pc_robot_lib_path$rosmaster_path $control_pc_ip_address $workstation_ip_address
-source $control_pc_robot_lib_path"/src/catkin_ws/devel/setup.zsh"
+source $control_pc_robot_lib_path"/catkin_ws/devel/setup.zsh"
 roslaunch franka_action_lib franka_ros_interface.launch
 EOSSH
 else
 sshpass -p "!@m-guardians" ssh -tt -o StrictHostKeyChecking=no $control_pc_uname@$control_pc_ip_address << EOSSH
-source ~/Documents/robot-interface/src/catkin_ws/src/franka_action_lib/scripts/bash_scripts/set_rosmaster.sh $control_pc_ip_address $workstation_ip_address
-source ~/Documents/robot-interface/src/catkin_ws/devel/setup.bash
+source ~/Documents/robot-interface/bash_scripts/set_rosmaster.sh $control_pc_ip_address $workstation_ip_address
+source ~/Documents/robot-interface/catkin_ws/devel/setup.bash
 roslaunch franka_action_lib franka_ros_interface.launch
 EOSSH
 fi
