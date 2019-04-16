2deea7886789588fa9ed8c460c92afb781ea5763
Mohit <mohit.sharma0690@gmail.com>
2019-04-16 18:22:23 -0400

    Add option to stop robolib on error.

--- bash_scripts/start_iam_robolib_on_control_pc.sh
+++ bash_scripts/start_iam_robolib_on_control_pc.sh
@@ -5,6 +5,7 @@ control_pc_ip_address=$2
 control_pc_robolib_path=$3
 control_pc_use_password=$4
 control_pc_password=$5
+robolib_stop_on_error=$6
 
 
 echo $control_pc_uname
@@ -12,19 +13,26 @@ echo $control_pc_ip_address
 echo $control_pc_robolib_path
 echo $control_pc_use_password
 echo $control_pc_password
+echo "Robolib will stop on error: "$robolib_stop_on_error
+
+stop_on_error=0
+if [ "$robolib_stop_on_error" = "0" ]; then
+    stop_on_error=1
+fi
+
 
 if [ "$control_pc_use_password" = "0" ]; then
 ssh -tt $control_pc_uname@$control_pc_ip_address << EOSSH
 cd $control_pc_robolib_path
 cd build
-./main_iam_robolib
+./main_iam_robolib --stop_on_error $stop_on_error
 bash
 EOSSH
 else
 sshpass -p "$control_pc_password" ssh -tt -o StrictHostKeyChecking=no $control_pc_uname@$control_pc_ip_address << EOSSH
 cd $control_pc_robolib_path
 cd build
-./main_iam_robolib
+./main_iam_robolib --stop_on_error $stop_on_error
 bash
 EOSSH
 fi
