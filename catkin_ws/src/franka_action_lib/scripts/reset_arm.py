f556912d8b67c3b781fe1475e35f506b59bc2af4
Jacky Liang <jackyliang@cmu.edu>
2019-12-19 08:24:01 -0500

    add close gripper option to reset arm script

--- catkin_ws/src/franka_action_lib/scripts/reset_arm.py
+++ catkin_ws/src/franka_action_lib/scripts/reset_arm.py
@@ -4,6 +4,7 @@ from frankapy import FrankaArm
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--use_joints', '-u', action='store_true')
+    parser.add_argument('--close_gripers', '-c', action='store_true')
     args = parser.parse_args()
 
     print('Starting robot')
@@ -16,5 +17,9 @@ if __name__ == '__main__':
         print('Reset with pose')
         fa.reset_pose()
     
-    print('Opening Grippers')
-    fa.open_gripper()
+    if args.close_gripers:
+        print('Closing Grippers')
+        fa.close_gripper()
+    else:
+        print('Opening Grippers')
+        fa.open_gripper()
