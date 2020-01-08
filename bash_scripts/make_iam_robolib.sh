e6f7b756aec7c3252a5e1f047231fddeaf1c4bb1
Sclee711 <stevenl3@andrew.cmu.edu>
2020-01-08 10:55:10 -0500

    changed bash to give warnings

--- bash_scripts/make_iam_robolib.sh
+++ bash_scripts/make_iam_robolib.sh
@@ -4,5 +4,5 @@ n_cores=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
 [ -d build ] || mkdir build
 cd build
 # make and ignore unused variable flags
-cmake -j$n_cores -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wno-unused-variable -Wno-unused-parameter -Wno-maybe-uninitialized" .. && make
+cmake -j$n_cores -DCMAKE_BUILD_TYPE=Release .. && make
 cd ..
